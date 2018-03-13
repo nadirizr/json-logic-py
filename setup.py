b29054ce446b6fdda1c9a655650334c3b342afc4
mmniaziqb <mohsin.niazi@qubit.com>
2018-03-13 12:30:33 +0500

    updated repo url

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/nadirizr/json-logic-py',
+    url='https://github.com/qubitdigital/json-logic-py',
 
     # Author details
     author='nadir.izr',
