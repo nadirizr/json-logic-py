4cd12266e762b0527431958d21bcb279dd454c6b
Sergei Maertens <sergei@maykinmedia.nl>
2022-12-22 17:14:28 +0100

    :sparkles: Implement parsing and displaying JSON logic expressions

--- json_logic/meta.py
+++ json_logic/meta.py
@@ -8,12 +8,14 @@ potential to (statically) analyze JSON logic expressions.
 
 from dataclasses import dataclass, field
 from textwrap import indent
-from typing import Union
+from typing import Union, cast
 
 from . import operations
-from .typing import Primitive
+from .typing import JSON, Primitive
 
 OperationArgument = Union[Primitive, "Operation"]
+
+NormalizedExpression = dict[str, list[JSON]]
 
 
 def _repr_arg(arg: "OperationArgument", prefix: str) -> str:
@@ -44,9 +46,10 @@ class Operation:
 
     Evaluation happens depth-first in case an argument is an operation itself.
     """
+    _check_registered: bool = field(init=False, default=True)
 
     def __post_init__(self):
-        if self.operator not in operations:
+        if self._check_registered and self.operator not in operations:
             raise ValueError(
                 f"Operator '{self.operator}' is unknown (unregistered in "
                 "'json_logic.operations')."
@@ -64,3 +67,79 @@ class Operation:
         if args_repr:
             op_repr = f"{op_repr}\n{args_repr}"
         return op_repr
+
+    @classmethod
+    def for_operator(cls, operator: str, *args, **kwargs):
+        operator_cls = OPERATION_MAP.get(operator, cls)
+        return operator_cls(operator, *args, **kwargs)
+
+
+class Var(Operation):
+    _check_registered = False
+
+    def __repr__(self):
+        return f"${self.arguments[0]}"
+
+
+def destructure(expression: NormalizedExpression) -> tuple[str, list[JSON]]:
+    """
+    Decompose a normalized expression into the operator and arguments.
+    """
+    # TODO: reliable way to read/extract the operator if we add extensions
+    # to expressions
+    operator = list(expression.keys())[0]
+    values = expression[operator]
+    return (operator, values)
+
+
+@dataclass
+class JSONLogicExpression:
+    expression: NormalizedExpression | Primitive | list[JSON]
+
+    @staticmethod
+    def normalize(expression: JSON) -> NormalizedExpression | Primitive | list[JSON]:
+        """
+        Remove syntactic sugar for unary operators.
+
+        Normalization happens only on the provided expression, not on nested
+        expressions inside.
+        """
+        # we only normalize one level at a time
+        if isinstance(expression, (list, Primitive)):
+            return cast(Primitive | list, expression)
+
+        assert isinstance(expression, dict)
+
+        operator, values = destructure(cast(dict, expression))
+        if not isinstance(values, list):
+            values = [values]
+
+        # make sure to keep any additional extension keys
+        return cast(NormalizedExpression, {**expression, operator: values})
+
+    @classmethod
+    def from_expression(cls, expression: JSON):
+        normalized = cls.normalize(expression)
+        return cls(normalized)
+
+    def as_tree(self):
+        """
+        Convert the JSON expression into a tree with Operation nodes.
+        """
+        if isinstance(self.expression, Primitive):
+            return self.expression
+
+        if isinstance(self.expression, list):
+            return [self.from_expression(child).as_tree() for child in self.expression]
+
+        assert isinstance(self.expression, dict)
+        operator, values = destructure(self.expression)
+        arguments = [
+            JSONLogicExpression.from_expression(value).as_tree() for value in values
+        ]
+        return Operation.for_operator(operator, arguments=arguments)
+
+
+OPERATION_MAP = {
+    "var": Var,
+}
--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -2,7 +2,7 @@ from textwrap import dedent
 
 import pytest
 
-from json_logic.meta import Operation
+from json_logic.meta import JSONLogicExpression, Operation, Var
 
 
 def test_representation_simple_operation():
@@ -35,6 +35,68 @@ def test_representation_nested_operations():
     assert repr(op2) == expected
 
 
+def test_representation_var():
+    var_foo = Var("var", ["foo"])
+
+    assert repr(var_foo) == "$foo"
+
+
 def test_unknown_operator():
     with pytest.raises(ValueError):
         Operation("some-wordsalad that is highly unlikely to be an operation")
+
+
+@pytest.mark.parametrize(
+    "expr",
+    (None, "foo", 42, 0.05, True, False),
+)
+def test_expression_parser_primitives(expr):
+    expression = JSONLogicExpression.from_expression(expr)
+
+    assert expression.expression == expr
+
+
+def test_expression_parser_simple_operation():
+    expression = JSONLogicExpression.from_expression({"var": ["foo"]})
+
+    assert expression.expression == {"var": ["foo"]}
+
+
+def test_expression_parser_simple_operation_syntactic_sugar():
+    expression = JSONLogicExpression.from_expression({"var": "foo"})
+
+    assert expression.expression == {"var": ["foo"]}
+
+
+def test_parse_simple_expression_into_tree():
+    expression = JSONLogicExpression.from_expression({"var": ["foo"]})
+
+    tree = expression.as_tree()
+
+    expected_tree = Var("var", ["foo"])
+    assert tree == expected_tree
+
+
+def test_complex_expression_into_tree_with_representation():
+    complex_reduce = {
+        ">": [
+            {"reduce": [{"var": "kinderen"}, {"+": [{"var": "accumulator"}, 1]}, 0]},
+            1,
+        ]
+    }
+    expression = JSONLogicExpression.from_expression(complex_reduce)
+    tree = expression.as_tree()
+
+    expected_repr = dedent(
+        """
+        Operation(>)
+          ├─ Operation(reduce)
+          │    ├─ $kinderen
+          │    ├─ Operation(+)
+          │    │    ├─ $accumulator
+          │    │    └─ 1
+          │    └─ 0
+          └─ 1
+        """
+    ).strip()
+    assert repr(tree) == expected_repr
